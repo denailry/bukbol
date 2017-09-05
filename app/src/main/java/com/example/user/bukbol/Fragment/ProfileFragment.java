e0a04dfaa99de8dced0c931abfb3d790ab2c3909
adylanrff <adylanrff@gmail.com>
2017-09-06 06:37:30 +0700

    profile done

--- app/src/main/java/com/example/user/bukbol/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/user/bukbol/Fragment/ProfileFragment.java
@@ -1,19 +1,31 @@
 package com.example.user.bukbol.Fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.example.user.bukbol.DetailProfileActivity;
+import com.example.user.bukbol.ProfileItem;
 import com.example.user.bukbol.R;
+import com.example.user.bukbol.adapter.ProfileAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
 
+    private ListView listView;
+    private ArrayList<ProfileItem> profileItems;
+    private ProfileAdapter adapter;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -21,10 +33,49 @@ public class ProfileFragment extends Fragment {
 
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        profileItems = new ArrayList<>();
+
+
+        profileItems.add(new ProfileItem("Personal Details",R.drawable.ic_person));
+        profileItems.add(new ProfileItem("Help",R.drawable.ic_help));
+        profileItems.add(new ProfileItem("Notifications",R.drawable.ic_notification));
+        profileItems.add(new ProfileItem("Settings",R.drawable.ic_settings));
+
+        adapter = new ProfileAdapter(getContext(),profileItems);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        listView = (ListView) v.findViewById(R.id.profile_lv);
+        listView.setAdapter(adapter);
+
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+                ProfileItem currentItem = profileItems.get(i);
+
+                switch (currentItem.getItemName()){
+
+                    case "Help":{
+                        Intent intent = new Intent(getActivity(),DetailProfileActivity.class);
+                        startActivity(intent);
+                        break;
+                    }
+                }
+            }
+        });
+        return v;
     }
 
 }
---
+++ app/src/main/java/com/example/user/bukbol/adapter/ProfileAdapter.java
@@ -0,0 +1,53 @@
+package com.example.user.bukbol.adapter;
+
+import android.content.Context;
+import android.media.Image;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.user.bukbol.ProfileItem;
+import com.example.user.bukbol.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Adylan Roaffa on 9/6/2017.
+ */
+
+public class ProfileAdapter extends ArrayAdapter<ProfileItem> {
+
+
+    public ProfileAdapter(Context context, ArrayList<ProfileItem> profileItems){
+        super(context,0,profileItems);
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItemView = convertView;
+
+        if (listItemView==null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.profile_item,parent,false);
+        }
+
+        final ProfileItem currentItem = getItem(position);
+        TextView itemName = (TextView) listItemView.findViewById(R.id.profile_item_name);
+        itemName.setText(currentItem.getItemName());
+
+        ImageView itemImage = (ImageView) listItemView.findViewById(R.id.profile_item_image);
+        itemImage.setImageResource(currentItem.getImageResource());
+
+
+        return listItemView;
+    }
+}
